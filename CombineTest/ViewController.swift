271513bf393c1409dc6fa653d6d36e344ae21df7
willtellis <will.ellis@willowtreeapps.com>
2020-10-30 16:39:13 -0400

    Use combine for API call

--- CombineTest/ViewController.swift
+++ CombineTest/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UITableViewController {
 
@@ -14,28 +15,45 @@ class ViewController: UITableViewController {
     typealias ViewModel = ViewControllerViewModel
     private var viewModelGenerator = ViewControllerViewModelGenerator()
     private var viewModel: ViewModel? = nil
+
+    // Token for paging from the RedditAPIClient
     private var after: String? = nil
+
+    private var cancellables = Set<AnyCancellable>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        apiClient.getPosts(after: nil) { result in
-            DispatchQueue.main.async { [weak self] in
-                guard let self = self else {
-                    return
-                }
-                guard
-                    let posts = try? result.get(),
-                    let viewModel = self.viewModelGenerator.make(with: posts)
-                else {
-                    // TODO: error screen
-                    return
-                }
-                self.after = posts.after
+        let postsPublisher = apiClient.getPostsPublisher(after: nil)
+            .multicast { PassthroughSubject<PostsAPIResponse, Error>() }
+
+        postsPublisher
+            .map { posts -> String? in
+                posts.after
+            }
+            .catch { error in
+                Just(nil)
+            }
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.after, on: self)
+            .store(in: &cancellables)
+
+        postsPublisher
+            .receive(on: DispatchQueue.main)
+            .map { posts in
+                return self.viewModelGenerator.make(with: posts)
+            }
+            .catch { error in
+                Just(self.viewModelGenerator.make(with: error))
+            }
+            .sink { viewModel in
                 self.render(viewModel)
             }
-        }
+            .store(in: &cancellables)
 
+        postsPublisher
+            .connect()
+            .store(in: &self.cancellables)
     }
 
     private func render(_ viewModel: ViewModel) {
--- CombineTest/ViewControllerViewModelGenerator.swift
+++ CombineTest/ViewControllerViewModelGenerator.swift
@@ -9,15 +9,16 @@ import Foundation
 
 struct ViewControllerViewModelGenerator {
 
-    func make(with model: PostsAPIResponse?) -> ViewControllerViewModel? {
-        guard let model = model else {
-            return nil
-        }
+    func make(with model: PostsAPIResponse) -> ViewControllerViewModel {
         let posts = model.data?.children?.compactMap({ makePost(with: $0) }) ?? []
         return ViewControllerViewModel(posts: posts)
     }
 
-    private func makePost(with model: PostsAPIResponse.Data.Child?) -> ViewControllerViewModel.Post? {
-        return ViewControllerViewModel.Post(title: model?.data?.title)
+    func make(with error: Error) -> ViewControllerViewModel {
+        return ViewControllerViewModel(posts: [])
+    }
+
+    private func makePost(with model: PostsAPIResponse.Data.Child) -> ViewControllerViewModel.Post? {
+        return ViewControllerViewModel.Post(title: model.data?.title)
     }
 }
